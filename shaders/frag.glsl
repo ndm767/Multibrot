ae41ffc238f2aa19cddf488829f445448d272c2a
ndm767 <5031736+ndm767@users.noreply.github.com>
2021-07-07 10:55:13 -0400

    added basic multibrot rendering

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,5 +1,49 @@
 #version 330 core
 
+in vec3 fPos;
+
 out vec4 outColor;
 
-void main() { outColor = vec4(1.0, 0.0, 0.0, 1.0); }
+const float MAX_ITER = 100.0;
+
+vec2 compMult(vec2 a, vec2 b) {
+    vec2 ret = vec2(0, 0);
+    ret.x = a.x * b.x - a.y * b.y;
+    ret.y = a.x * b.y + a.y * b.x;
+    return ret;
+}
+
+vec2 compExp(vec2 z, float power) {
+    if (power <= 0) {
+        return vec2(0, 0);
+    }
+
+    vec2 ret = z;
+    for (float i = 1; i < power; i++) {
+        ret = compMult(ret, z);
+    }
+
+    return ret;
+}
+
+float multibrot(vec2 c, float power, float iter) {
+    vec2 z = vec2(0, 0);
+    for (float i = 0; i < MAX_ITER; i++) {
+
+        z = compExp(z, power) + c;
+
+        if (length(z) > 2)
+            return i / iter;
+        if (i >= iter) {
+            break;
+        }
+    }
+
+    return iter;
+}
+
+void main() {
+    vec2 compPos = vec2(fPos.xy);
+    float val = multibrot(compPos, 3.0, 10.0);
+    outColor = vec4(val, val, val, 1.0);
+}
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -2,4 +2,9 @@
 
 layout(location = 0) in vec3 pos;
 
-void main() { gl_Position = vec4(pos, 1.0); }
+out vec3 fPos;
+
+void main() {
+    fPos = pos;
+    gl_Position = vec4(pos, 1.0);
+}
